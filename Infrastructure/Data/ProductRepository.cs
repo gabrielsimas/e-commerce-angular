9abe308ae9315f2e5834b7936e025b2ae77111a2
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-06-19 20:14:21 -0300

    Eager loading of navigation properties

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -13,16 +13,31 @@ namespace Infrastructure.Data
         {
             _context = context;
 
-        }
+        }        
 
         public async Task<Product> GetProductByIdAsync(int id)
-        {
-            return await _context.Products.FindAsync(id);
-        }
+            => await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id.Equals(id));
+        
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
-        {
-            return await _context.Products.ToListAsync();
-        }
+            => await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
+            => await _context.ProductBrands.ToListAsync();
+
+        public async Task<ProductBrand> GetProductBrandsByIdAsync(int id)
+            => await _context.ProductBrands.FindAsync(id);
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
+            => await _context.ProductTypes.ToListAsync();
+
+        public async Task<ProductType> GetProductTypesByIdAsync(int id)
+            => await _context.ProductTypes.FindAsync(id);
     }
 }
