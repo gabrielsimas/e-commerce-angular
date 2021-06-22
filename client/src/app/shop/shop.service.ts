8aaaa98a252cdcab64421b4ecd1494eac118f9ac
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-06-22 11:23:09 -0300

    Adding the filter functionality

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,8 +1,9 @@
 import { IProductBrand } from './../shared/models/productBrand';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
 import { IProductType } from '../shared/models/productType';
+import {delay, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,24 @@ export class ShopService {
 
   }
 
-  getProducts() {
-    return this.http.get<IPagination>(`${this.baseUrl}/products?pageSize=50`);
+  getProducts(brandId?: number, typeId?:number) {
+    let params = new HttpParams();
+
+    if(brandId) {
+      params = params.append('brandId', brandId.toString());
+    }
+
+    if(typeId) {
+      params = params.append('typeId', typeId.toString());
+    }
+
+    return this.http.get<IPagination>(`${this.baseUrl}/products`, {observe: 'response', params})
+      .pipe(
+        delay(1000),
+        map(response => {
+          return response.body;
+        })
+      );
   }
 
   getBrands() {
