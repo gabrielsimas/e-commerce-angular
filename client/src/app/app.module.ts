aae51459515b031bd9f4b496f10d941171b38365
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-06-27 19:34:03 -0300

    Using the https Interceptor to catch errors

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,13 @@
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
@@ -21,7 +21,9 @@ import { ShopModule } from './shop/shop.module';
     CoreModule,    
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent] //Módulo que inicializa a aplicação
 })
 export class AppModule { }
