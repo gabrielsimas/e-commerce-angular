02cdb8aee92690d8650c961f50e2b437c06e1814
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-07-23 09:05:24 -0300

    Adding breadcrumbs

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
@@ -16,6 +17,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
   imports: [
     CommonModule,
     RouterModule,
+    BreadcrumbModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
