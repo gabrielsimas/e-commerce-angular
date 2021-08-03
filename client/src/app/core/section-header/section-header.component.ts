b054d8c1920f17442b890530e83dbc4b1cac016a
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-08-03 11:48:30 -0300

    127. Setting the section header title from

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { BreadcrumbService } from 'xng-breadcrumb';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
+  breadcrumb$: Observable<any[]>;
 
-  constructor() { }
+  constructor(private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
+    this.breadcrumb$ = this.bcService.breadcrumbs$;
   }
 
 }
