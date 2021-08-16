01619e9cbd3c898b63b3471a7dc35586db660b96
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-08-16 17:28:44 -0300

    146. Creating the basket module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { BasketComponent } from './basket/basket.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'not-found', component: NotFoundComponent, data: {breadcrumb: 'Not Found'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule), data: {breadcrumb: 'Shop'}},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule), data: {breadcrumb: 'BasketComponent'}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
