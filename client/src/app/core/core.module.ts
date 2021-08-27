0803b8bf155c2125b2bffdfc119eb792142cb40b
Luis Gabriel N. Simas <gabrielsimas@gmail.com>
2021-08-27 13:14:30 -0300

    Creating and error component for testing the error responses

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent],
   imports: [
     CommonModule,
     RouterModule
