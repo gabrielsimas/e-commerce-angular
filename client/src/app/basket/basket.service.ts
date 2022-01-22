caefc5890fb833d0beaa60904dfe7bd095869bbc
Luis Gabriel N. Simas <gabrielsimas@gmail.com>
2022-01-22 08:01:05 -0300

    246. Updating the shipping price

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,3 +1,4 @@
+import { IDeliveryMethod } from './../shared/models/deliveryMethod';
 import { IProduct } from './../shared/models/product';
 import { Basket, IBasket, IBasketItem, IBasketTotals } from './../shared/models/basket';
 import { HttpClient } from '@angular/common/http';
@@ -16,8 +17,14 @@ export class BasketService {
   baseUrl = environment.apiUrl;
   basket$ = this.basketSource.asObservable();  
   basketTotal$ = this.basketTotalSource.asObservable();
+  shipping = 0;
 
   constructor(private http: HttpClient) { }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   getBasket(id: string) {
     return this.http.get(`${this.baseUrl}basket?id=${id}`)
@@ -91,7 +98,7 @@ export class BasketService {
 
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
-    const shipping = 0;
+    const shipping = this.shipping;
     const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a,0);
     const total = subtotal + shipping;
     this.basketTotalSource.next({shipping, total, subtotal});
--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,3 +1,4 @@
+import { BasketService } from 'src/app/basket/basket.service';
 import { IDeliveryMethod } from './../../shared/models/deliveryMethod';
 import { CheckoutService } from './../checkout.service';
 import { Component, Input, OnInit } from '@angular/core';
@@ -12,7 +13,7 @@ export class CheckoutDeliveryComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
   deliveryMethods: IDeliveryMethod[];
   
-  constructor(private checkoutService: CheckoutService) { }
+  constructor(private checkoutService: CheckoutService, private basketService: BasketService) { }
 
   ngOnInit(): void {
     this.checkoutService.getDeliveryMethods().subscribe((dm: IDeliveryMethod[]) => {
@@ -20,6 +21,10 @@ export class CheckoutDeliveryComponent implements OnInit {
     }, error => {
       console.log(error);
     })
+  }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.basketService.setShippingPrice(deliveryMethod);
   }
 
 }
